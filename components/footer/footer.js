71e8d2808182736899ebb20c04d7b28d00afbda5
Daniel Gaviria <danielfgaviria@gmail.com>
2022-04-18 22:50:37 -0400

    added bio, added header component, changed items nav/footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -12,7 +12,6 @@ class Footer extends HTMLElement {
             <footer class="py-3 my-4">
              <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                  <li class="nav-item"><a href="https://dfgav1r1a.github.io/" class="nav-link px-2 text-muted">Home</a></li>
-                 <li class="nav-item"><a href="http://www.danielgaviria.net/artist/daniel-gaviria/" target="_blank" class="nav-link px-2 text-muted">Bio</a></li>
                  <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Projects</a></li>
                  <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contact</a></li>       
              </ul>
