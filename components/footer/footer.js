a94f51fb6681b2ddd7bc7f2aa396587d093abc6d
Daniel Gaviria <danielfgaviria@gmail.com>
2022-04-20 11:58:30 -0400

    edited footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -12,8 +12,8 @@ class Footer extends HTMLElement {
             <footer class="py-3 my-4">
              <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                  <li class="nav-item"><a href="https://dfgav1r1a.github.io/" class="nav-link px-2 text-muted">Home</a></li>
-                 <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Projects</a></li>
-                 <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contact</a></li>       
+                 <li class="nav-item"><a href="#navbarDropdown" class="nav-link px-2 text-muted">Projects</a></li>
+                 <li class="nav-item"><a href="mailto:dfgaviria.dev@gmail.com"" class="nav-link px-2 text-muted">Contact</a></li>       
              </ul>
              <p class="text-center text-muted">© 2022 Daniel Gaviria</p>
             </footer>
